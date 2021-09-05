ca204bb8921c5db3e7b22aa12b44c0ff0f86ce42
Ben Marley <benjaminjohnmarley@gmail.com>
2021-09-05 09:29:52 +0100

    tweak styles

--- src/App.js
+++ src/App.js
@@ -6,38 +6,62 @@ function App() {
     {
       url: "https://happymag.tv/wp-content/uploads/2020/01/dave-featre.jpg",
       title: "Happy",
-      keyId: "jsdgflisdudi78o8uyku",
+      keyId: "1jsdgflisdudi78o8uyku",
     },
     {
       url: "https://images.thebrag.com/td/uploads/2017/08/david-bowie-early-768x499.jpg",
       title: "Sad",
-      keyId: "jsdgfmisdudi78o8uyku",
+      keyId: "2jsdgfmisdudi78o8uyku",
     },
     {
       url: "https://www.davidbowieworld.nl/wp-content/uploads/2019/11/DAVID-BOWIE-TIRED-OF-MY-LIFE-EP.jpg",
       title: "Tired",
-      keyId: "jsdgflisdudi78o8ullku",
+      keyId: "3jsdgflisdudi78o8ullku",
     },
     {
       url: "https://www.telegraph.co.uk/content/dam/tv/2017/06/13/david-bowie-twin-peaks_trans_NvBQzQNjv4BqD19gJsrS5dVDZFTjDrjdaMP3SSbqKuTAt_OcINYzAxU.jpg?imwidth=450",
-      title: "Confused",
-      keyId: "jsdgflisdnndi78o8uyku",
+      title: "Anxious",
+      keyId: "4jsdgflisdnndi78o8uyku",
     },
     {
-      url: "https://www.gannett-cdn.com/-mm-/a624164c6135ff8b124c5dd7670eeb6481c911ca/c=0-98-979-651/local/-/media/2015/07/20/Rochester/Rochester/635730005445868132-David-Bowie-mugshot.jpg?auto=webp&format=pjpg&width=1200",
-      title: "Rebellious",
-      keyId: "jzdgflisdudi78o8uyku",
+      url: "https://uploads4.wikiart.org/00282/images//unnamed.jpg!Portrait.jpg",
+      title: "Confused",
+      keyId: "88jzdgflisdudi78o8uyku",
     },
-    { url: "https://via.placeholder.com/150", title: "Ecstatic" },
+    {
+      url: "https://www.irishtimes.com/polopoly_fs/1.2931913.1484065196!/image/image.jpg",
+      title: "Angry",
+      keyId: "88jzdgflisdudi78o8uyku",
+    },
+    {
+      url: "https://img.buzzfeed.com/buzzfeed-static/static/2014-10/7/13/enhanced/webdr04/anigif_enhanced-buzz-9375-1412701355-31.gif",
+      title: "Excited",
+      keyId: "5jzdgflisdudi78o8uyku",
+    },
+    {
+      url: "https://img.buzzfeed.com/buzzfeed-static/static/2014-10/7/12/enhanced/webdr10/anigif_enhanced-buzz-12323-1412700435-12.gif",
+      title: "Annoyed",
+      keyId: "6jzdgflisdudi78o8uyku",
+    },
     {
       url: "https://i.guim.co.uk/img/media/eabb335850da800d98b858bc7117b14dc0dba46e/0_231_4000_2399/master/4000.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=a333003f3918df0f7706a34ce9fc1c3b",
       title: "Menacing",
-      keyId: "jsdgblisdudi78o8uyku",
+      keyId: "7sdgblisdudi78o8uyku",
     },
     {
       url: "https://walrus-assets.s3.amazonaws.com/img/ThinkingMan%E2%80%99sBalladeer.jpg",
       title: "Pensive",
-      keyId: "jsdgflisdcdi78o8uyku",
+      keyId: "8sdgflisdcdi78o8uyku",
+    },
+    {
+      url: "https://mtv.mtvnimages.com/uri/mgid:ao:image:mtv.com:63735?quality=0.8&format=jpg",
+      title: "Bad Hair Day",
+      keyId: "097sdgblisdudi78o8uyku",
+    },
+    {
+      url: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2020/04/15/11/david-bowie.jpg?width=990&auto=webp&quality=75",
+      title: "Drum and Bass Phase",
+      keyId: "79sdgflisdcdi78o8uyku",
     },
   ];
 
@@ -54,20 +78,24 @@ function App() {
           {" "}
           Sprint Retro (David Bowie remix)
         </marquee>
+
         <marquee
           className="app__heading"
-          behavior="scroll"
           direction="down"
-          Scrollamount="10"
+          height="120"
+          behavior="alternate"
         >
-          Pick your Bowie
+          <marquee behavior="alternate">
+            <span className="neon__yellow">Pick</span>
+            <span className="neon__red">Your</span>
+            <span className="neon__blue">Bowie</span>
+          </marquee>
         </marquee>
+
         <Fragment>
           <div className="bowie__cards-container">
             <ul className="bowie__cards-list">
               {bowieData.map((item) => {
-                console.log("item", item.url);
-
                 return (
                   <div className="bowie__card" key={item.keyId}>
                     <div
@@ -78,9 +106,7 @@ function App() {
                         backgroundSize: "cover",
                         backgroundRepeat: "no-repeat",
                       }}
-                    >
-                      {/* <img src={item.url} /> */}
-                    </div>
+                    ></div>
                     <div className="card__meta-container">
                       <p className="card__meta-title">{item.title}</p>
                     </div>
